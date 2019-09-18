422d272ef546a7dc5aed68ba245e7f7188957e9a
serena Lee <dearserenalee@gmail.com>
2019-09-18 02:55:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function dwarfRollCall(dwarves) {
+  let dwarveNames = [];
+  for(let i = 0; i < dwarves.length; i++){
+    dwarveNames.push(`${i + 1}. ${dwarves[i]} `)
+  }
+  return dwarveNames.join('');
 }
 
-function summonCaptainPlanet(planeteerCalls){
+
+ function summonCaptainPlanet(planeteerCalls){
+   let summonCall = [];
+   for(let i = 0; i< planeteerCalls.length; i++){
+     summonCall.push(`${planeteerCalls[i].toUpperCase()}!`);
+   }
+   return summonCall;
 }
+
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i< words.length; i++){
+    if(words[i].length >4){
+      return true;
+    }
+    else {return false}
+  }
 }
 
+
 function findTheCheese (foods) {
+	for (var i = 0; i < foods.length; i++) {
+		if (foods[i] === "cheddar") {
+		return "cheddar";
+		} else if (foods[i] === "gouda") {
+		return "gouda";
+		} else if (foods[i] === "camembert") {
+		return "camembert";
+		}
+	}
+	return "no cheese!";
 }
